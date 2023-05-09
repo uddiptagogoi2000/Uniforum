4e3c8fa9fc8a2d5c91e52565e30acbe791325f03
Uddipta Gogoi <uddiptagogoi2000@gmail.com>
2023-05-09 17:08:11 +0530

    setup react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to Uniforum</h1>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
