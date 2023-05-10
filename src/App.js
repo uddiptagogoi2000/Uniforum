179ad31a570f136662004f44b80b19a8918eb9a3
Uddipta Gogoi <uddiptagogoi2000@gmail.com>
2023-05-10 15:47:31 +0530

    here we go again says chakra

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
 const App = () => {
   return (
     <>
@@ -9,5 +6,4 @@ const App = () => {
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+export default App;
